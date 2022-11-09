34baf50848d4fbf30c143c0c79261d5ea5334069
0oLiHano0 <110515389+0oLiHano0@users.noreply.github.com>
2022-11-09 11:36:07 +0800

    Update trojan-go.sh

--- trojan-go.sh
+++ trojan-go.sh
@@ -420,7 +420,7 @@ getCert() {
             systemctl start cron
             systemctl enable cron
         fi
-        curl -sL https://get.acme.sh | sh -s email=hansenli@outlook.com
+        curl -sL https://get.acme.sh
         source ~/.bashrc
         ~/.acme.sh/acme.sh  --upgrade  --auto-upgrade
         ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
