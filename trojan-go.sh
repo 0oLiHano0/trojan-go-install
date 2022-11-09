2df30bb1f3b9c3fa48fba788fba27598a3589b97
0oLiHano0 <110515389+0oLiHano0@users.noreply.github.com>
2022-11-09 11:18:20 +0800

    Update trojan-go.sh

--- trojan-go.sh
+++ trojan-go.sh
@@ -11,7 +11,7 @@ PLAIN='\033[0m'
 OS=`hostnamectl | grep -i system | cut -d: -f2`
 
 # V6_PROXY=""
-# IP=`curl -sL -4 ip.sb`
+ IP=`curl -sL -4 ip.sb`
 # if [[ "$?" != "0" ]]; then
 #     IP=`curl -sL -6 ip.sb`
 #     V6_PROXY="https://hans3n.top/"
